f4a6d66be99cac3b74d1b02fd943ba7b733bf82e
Belicio Batista Cardoso <44432451+belloinfo@users.noreply.github.com>
2020-03-04 06:02:26 -0300

    conrrigido api sendgrid e testando api sendiblue

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,7 +1,15 @@
 'user strict';
 
-var config = require('../config');
-var sendgrid = require('sendgrid')(config.sendgridkey);
+const config = require('../config');
+const sendgrid = require('@sendgrid/mail');
+sendgrid.setApiKey(config.sendgridkey);
+
+//# Include the SendinBlue library
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+var defaultClient = SibApiV3Sdk.ApiClient.instance;
+//Instantiate the client
+var apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = config.sendinbluekey;
 
 exports.send = async (to, subject, body) => {
 	sendgrid.send({
